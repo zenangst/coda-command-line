73c8bfa94be5929d8d6dddf3bd4aa32815a1f88d
Christoffer Winterkvist <chris@zenangst.com>
2014-06-30 20:15:44 +0200

    Initial Commit

---
+++ coda/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  coda
+//
+//  Created by Christoffer Winterkvist on 6/30/14.
+//  Copyright (c) 2014 zenangst. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
