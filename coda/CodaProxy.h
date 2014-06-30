3497da6817bd005048515c8120082b4b1560660a
Christoffer Winterkvist <chris@zenangst.com>
2014-06-30 20:27:31 +0200

    Add CodaProxy class

---
+++ coda/CodaProxy.h
@@ -0,0 +1,14 @@
+//
+//  CodaProxy.h
+//  coda
+//
+//  Created by Christoffer Winterkvist on 6/30/14.
+//  Copyright (c) 2014 zenangst. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <ScriptingBridge/SBApplication.h>
+
+@interface CodaProxy : NSObject
+
+@end
---
+++ coda/CodaProxy.m
@@ -0,0 +1,13 @@
+//
+//  CodaProxy.m
+//  coda
+//
+//  Created by Christoffer Winterkvist on 6/30/14.
+//  Copyright (c) 2014 zenangst. All rights reserved.
+//
+
+#import "CodaProxy.h"
+
+@implementation CodaProxy
+
+@end
